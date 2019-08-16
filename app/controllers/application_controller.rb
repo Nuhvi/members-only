7f5356935b4863106bfaf00cec90cdd539bf580c
themonster2015 <quynhyen.vothi@gmail.com>
2019-08-16 16:27:57 +0200

    postcontroller create new post bla bla

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(user_id)  
+      @current_user ||= User.find_by(id: user_id)  
     elsif (remember_token = cookies[:remember_token])
-      user = User.find_by(remember_token)
+      user = User.find_by(remember_token:remember_token)
       log_in(user)
       @current_user = user
     end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,29 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  def index; end
+  #before_action :signed_in?, only: [:new, :create]
+  def index
+    @posts = Post.all 
+  end
 
-  def new; end
+  def new
+    @post = Post.new
+  end
 
-  def create; end
+  def create
+    post = current_user.posts.build(post_params)
+    if post.save
+      redirect_to root_url
+    else 
+      p "error creating post"
+      render :new
+    end
+  end
+  private
+  def signed_in?
+    current_user
+  end
+  def post_params
+    params.require(:post).permit(:title,:content)
+  end
 end
