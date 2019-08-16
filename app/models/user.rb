0663aa245aa5e0aae048a050ef327f131bc9197e
Nazeh <ar.nazeh@gmail.com>
2019-08-16 13:26:04 +0300

    started sessions and posts controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    validates :name, presence: true, length: {minimum: 6}
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates  :email, presence: true, uniqueness:  {case_sensitive: false }, format: {with: VALID_EMAIL_REGEX}
-    has_secure_password
-    validates :password, presence: true
+  validates :name, presence: true, length: { minimum: 6 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
+  validates :password, presence: true
 end
