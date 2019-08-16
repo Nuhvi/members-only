d89f62fc44f392ef915d5b7a12af954a1d7a0cd2
Nazeh <ar.nazeh@gmail.com>
2019-08-16 13:49:05 +0300

    started seed.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+
+
+  before_create :create_new_token
+
   validates :name, presence: true, length: { minimum: 6 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
   has_secure_password
   validates :password, presence: true
+
+
+  def create_new_token
+    new_token = SecureRandom.urlsafe_base64
+    encrypted_token = Digest::SHA1.hexdigest(SecureRandom.urlsafe_base64)
+    self.remember_encrypted = encrypted_token
+  end
 end
